f10f326d9f482811004344b9614b69a225563ed3
rwony <dev.rwlee@gmail.com>
2022-12-06 20:29:12 +0900

    🚧 Add color code of getFontColor() function

--- src/util/common.js
+++ src/util/common.js
@@ -7,12 +7,32 @@ export const checkEmptyString = (str) => {
 
 export const getFontColor = (color) => {
   switch (color) {
+    case 'white':
+      return '#fff'
+      break
+    case 'black':
+      return '#000'
+      break
+    case 'dark':
+      return '#191a20'
+      break
+    case 'primary':
+      return '#3f4150'
+      break
     case 'purple':
       return '#6c5ce7'
       break
-
+    case 'border':
+      return '#e0e2e7'
+      break
     case 'gray':
       return '#b2b3b9'
+      break
+    case 'background':
+      return '#f7f8fa'
+      break
+    case 'background-dark':
+      return '#2f3438'
       break
 
     default:
