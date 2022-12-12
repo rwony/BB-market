1301263b0c6a136339f9bb51d3366f302ff203bb
rwony <dev.rwlee@gmail.com>
2022-12-12 23:19:42 +0900

    🚧 Edit card css

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,39 +5,64 @@ import styled from 'styled-components'
 import { getFontColor } from '../util/common'
 
 const CardDiv = styled(Col)`
-  width: 300px;
+  width: 120px;
   height: auto;
   margin-bottom: 50px;
-  text-align: center;
+`
+const CardImageDiv = styled.div`
+  margin-bottom: 4px;
+  border-radius: 8px;
+  overflow: hidden;
+
+  &:hover {
+    transform: scale(1.05);
+    transition: transform 0.3s ease-in-out;
+  }
 `
 const CardImage = styled.img`
   display: block;
   width: 100%;
 `
 const CardTitle = styled.h4`
-  font-size: 16px;
+  display: -webkit-box;
+  font-size: 13px;
+  line-height: 18px;
+  letter-spacing: -0.05em;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
 `
 const CardDesc = styled.p`
   font-size: 13px;
-  color: ${getFontColor('gray')};
+  letter-spacing: -0.05em;
 `
 const CardDescStrong = styled.strong`
-  font-size: 14px;
-  letter-spacing: -0.02em;
+  color: ${getFontColor('dark')};
+  font-size: 15px;
+  margin-right: 1px;
 `
 
 const Card = (props) => {
   const item = props.data
+
   return (
-    <CardDiv sm={4} md={6} key={item.id}>
+    <CardDiv sm={4} key={item.id}>
+      <CardImageDiv>
+        <Link to={`/detail/${item.id}`}>
+          <CardImage src={item.thumbnail} alt={`${item.title} 이미지`} />
+        </Link>
+      </CardImageDiv>
+
       <Link to={`/detail/${item.id}`}>
-        <CardImage src={item.url} alt={item.title} />
-      </Link>
-      <Link to={'/detail'}>
         <CardTitle>{item.title}</CardTitle>
       </Link>
-      <CardDesc>{item.content}</CardDesc>
-      <CardDescStrong>￦ {item.price.toLocaleString()}</CardDescStrong>
+      <CardDesc>
+        <CardDescStrong>
+          {item.price === 0 ? null : item.price.toLocaleString()}
+        </CardDescStrong>
+        원
+      </CardDesc>
     </CardDiv>
   )
 }
