bbe2875ff6d018acacd8e68cf3dd286779c8db2d
rwony <dev.rwlee@gmail.com>
2022-12-12 23:18:42 +0900

    🚧 Edited product json data

--- src/util/shoesList.js
+++ src/util/shoesList.js
@@ -1,27 +1,39 @@
-const shoesData = [
+const productData = [
   {
     id: 1,
-    title: 'White and Black',
-    content: 'Born in France',
-    price: 120000,
-    url: 'https://codingapple1.github.io/shop/shoes1.jpg',
+    title: '애니유 펫밀크 애니유 | 강아지 고양이 우유 180ml 10개입',
+    brand: '애니유 AniU',
+    originalPrice: 24000,
+    price: 16500,
+    thumbnail:
+      'https://raw.githubusercontent.com/rwony/rwony.github.io/main/BB-market/products/AniU_thumb.jpg',
+    content:
+      'https://raw.githubusercontent.com/rwony/rwony.github.io/main/BB-market/products/AniU_content.png',
   },
 
   {
     id: 2,
-    title: 'Red Knit',
-    content: 'Born in Seoul',
-    price: 110000,
-    url: 'https://codingapple1.github.io/shop/shoes2.jpg',
+    title: '지위픽 독 간식 사슴고기 85g',
+    brand: '지위픽 ZIWI PEAK',
+    originalPrice: 19600,
+    price: 15700,
+    thumbnail:
+      'https://raw.githubusercontent.com/rwony/rwony.github.io/main/BB-market/products/Ziwipeak_thumb.jpg',
+    content:
+      'https://raw.githubusercontent.com/rwony/rwony.github.io/main/BB-market/products/Ziwipeak_content.jpg',
   },
 
   {
     id: 3,
-    title: 'Grey Yordan',
-    content: 'Born in the States',
-    price: 130000,
-    url: 'https://codingapple1.github.io/shop/shoes3.jpg',
+    title: '서루 하트펫백(스카이블루)',
+    brand: '서루 SEORU',
+    originalPrice: 0,
+    price: 116000,
+    thumbnail:
+      'https://raw.githubusercontent.com/rwony/rwony.github.io/main/BB-market/products/seoru_thumb.jpeg',
+    content:
+      'https://raw.githubusercontent.com/rwony/rwony.github.io/main/BB-market/products/seoru_content.png',
   },
 ]
 
-export default shoesData
+export default productData
